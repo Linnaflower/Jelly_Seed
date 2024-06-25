cbcc9384d78f0faca7eb07229eacbf14f7a1c771
Helinna <helinna74@gmail.com>
2024-06-25 02:51:40 -0400

    added event trigger via enter key and error handling

--- Weather/script.js
+++ Weather/script.js
@@ -1,22 +1,62 @@
 const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
+const searchEnter = document.querySelector('.search-box input');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
+const error = document.querySelector('.not-found');
+
+function execution(){
+    const location_ary = document.querySelector('.search-box input').value.split(', ');
+    //city, country format
+    console.log("passed");
+    return location_ary;
+}
+searchEnter.addEventListener('keypress', function(event){
+    if (event.key === 'Enter'){
+        event.preventDefault();
+        search.click();
+    }
+});
 
 search.addEventListener('click', () => {
+    const location = execution();
+    const city = location[0];
+    const country = location[1];
     const APIKey = 'dbe8c6f70868701b623c00ba64edd2ee';
-    const city = document.querySelector('.search-box input').value;
-    if (city == '') {
+    
+    if (city === '' && country === '') {
+        console.log("no city provided");
         return;
     }
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`)
-        .then(response => response.json())
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${APIKey}`)
+        .then(response => {
+            console.log("City receieved");
+            return response.json();
+        })
         .then(json => {
+            console.log("json recieved");
+
+            if (json.cod === '404'){
+                container.style.height = '400px';
+                weatherBox.classList.remove('active');
+                weatherDetails.classList.remove('active');
+                error.classList.add('active');
+                return;
+            }
+
+            container.style.height = '555px';
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            error.classList.remove('active');
+
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
+
+            // console.log(json.weather[0].main);
 
             switch (json.weather[0].main) {
                 case 'Clear':
@@ -41,6 +81,10 @@ search.addEventListener('click', () => {
                     image.src = 'images/cloudy.png';
                     break;
             }
-
+            
+            temperature.innerHTML =`${parseInt(json.main.temp)}<span>°F</span>`;
+            description.innerHTML =`${json.weather[0].description}`;
+            humidity.innerHTML =`${json.main.humidity}%`;
+            wind.innerHTML =`${parseInt(json.wind.speed)}mp/h`;
     });
 });
